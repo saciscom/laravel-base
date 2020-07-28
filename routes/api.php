be171cc78705774fba4bd646441b9b5019b54031
An Nguyen Thanh <annt@nal.vn>
2020-07-28 16:32:05 +0700

    [USER] Add logout api

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::post('/login', 'AuthenticateController@login')->name('auth.login');
 Route::post('/register', 'AuthenticateController@register')->name('auth.register');
 Route::get('/me', 'AuthenticateController@me')->name('auth.me')->middleware('auth.user');
+Route::post('/logout', 'AuthenticateController@logout')->name('auth.logout')->middleware('auth.user');
